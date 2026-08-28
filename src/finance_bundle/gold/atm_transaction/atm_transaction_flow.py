aec2999b0fd51943a262d923504a14149874b887
aishwaryaljadhav <aishwaryaljadhav18@gmail.com>
2026-08-28 14:38:36 +0530

    atm transaction changes

--- src/finance_bundle/gold/atm_transaction/atm_transaction_flow.py
+++ src/finance_bundle/gold/atm_transaction/atm_transaction_flow.py
@@ -1,39 +1,16 @@
 from pyspark.sql import DataFrame
 
 from finance_bundle.gold.atm_transaction.atm_transaction_transform import (
-    enrich_atm_transactions,
     build_atm_summary,
 )
 
 
 # ==========================================================
-# Gold ATM Transaction Flow
+# GOLD ATM TRANSACTION FLOW
 # ==========================================================
 
 def transform_gold_atm_summary(
     atm_df: DataFrame,
-    customer_df: DataFrame,
-    account_df: DataFrame,
-    card_df: DataFrame,
-    branch_df: DataFrame,
 ) -> DataFrame:
 
-    # ------------------------------------------------------
-    # Enrich ATM transactions
-    # ------------------------------------------------------
-
-    enriched_df = enrich_atm_transactions(
-        atm_df=atm_df,
-        customer_df=customer_df,
-        account_df=account_df,
-        card_df=card_df,
-        branch_df=branch_df,
-    )
-
-    # ------------------------------------------------------
-    # Build Gold ATM Summary
-    # ------------------------------------------------------
-
-    return build_atm_summary(
-        enriched_df
-    )
+    return build_atm_summary(atm_df)
